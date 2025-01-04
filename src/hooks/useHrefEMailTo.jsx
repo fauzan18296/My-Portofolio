8356fa63c65107f5f560425f2b92fbae6f8f6193
Ahmad Fauzan <fauzan18296@gmail.com>
2025-01-04 23:20:52 +0700

    delete(file): delete file in folder hooks

--- src/hooks/useHrefEMailTo.jsx
+++
@@ -1,9 +0,0 @@
-import { useState } from 'react'
-export const useHrefEMailTo = () => {
-   const [btnLocate, setBtnLocate] = useState("mailto:fauzan18296@gmail.com")
-
-  const handleBtnLocate = () => {
-    setBtnLocate(location.href = btnLocate )
-  }
-  return handleBtnLocate
-}
