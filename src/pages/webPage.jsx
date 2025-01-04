2f870f4446479a8bb8df3f811793830b30f49d06
Ahmad Fauzan <fauzan18296@gmail.com>
2025-01-04 22:15:44 +0700

    add(link): add link for send message to my email

--- src/pages/webPage.jsx
+++ src/pages/webPage.jsx
@@ -1,8 +1,6 @@
 import Header from '../components/Layouts/Header'
 import HeroSection from '../components/Ui/HeroSection'
-import Button from '../components/Ui/Button'
 import { Typewriter } from 'react-simple-typewriter'
-import { sendEmail } from '../utils/sendEmail'
 
 const WebPage = () => {
   const Introduction = ["Ahmad Fauzan", "a Programmer", "a Fresh Graduates"]
@@ -22,7 +20,7 @@ const WebPage = () => {
               delaySpeed={1000}
           /></span></h1>
           <p className='max-tablet:text-md'>On this website I will share information about my skills, about me, information about the website projects I have created in front-end development, and etc. Then i will work on my personal project and also will share it here as my portfolio.</p>
-           <Button classname='flex bg-indigo-400 px-2 py-1 text-white rounded-md shadow-md mt-2 max-tablet:text-md' onClick={sendEmail}>Hire Me!</Button>
+           <a className='flex bg-indigo-400 px-2 py-1 text-white rounded-md shadow-md mt-2 max-tablet:text-md' href='mailto:fauzan18296@gmail.com'>Hire Me!</a>
         </div>
       </HeroSection>
       </>
