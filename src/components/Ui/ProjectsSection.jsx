b3db6a3b48b18bc056746c590dc9d969485199d5
Ahmad Fauzan <fauzan18296@gmail.com>
2025-06-11 15:24:54 +0700

    style: Change Style column on device mobile in projects section

--- src/components/Ui/ProjectsSection.jsx
+++ src/components/Ui/ProjectsSection.jsx
@@ -8,8 +8,8 @@ const ProjectsSection = () => {
     <div id="project" className="scroll-mt-20 min-h-dvh">
       <h1 className="text-2xl text-indigo-400 mb-1 font-bold text-center max-mobile:text-lg">My Projects</h1>
       <div className="container flex justify-center items-center">
-        <div className='flex justify-center items-center gap-5'>
-          <div className='grid grid-rows-2 max-mobile:flex max-mobile:flex-col'></div>
+        <div className='flex justify-center items-center max-mobile:flex-col gap-5'>
+          <div className='grid grid-cols-2'></div>
        <Link className='flex gap-2' to={'/websites/projects'}><NavigationIcon icon='link' /><h1 className='text-xl font-bold max-mobile:text-lg'> Websites Projects</h1></Link>
        <Link className='flex gap-2' to={'/websites/projects'}><NavigationIcon icon='link' /><h1 className='text-xl font-bold max-mobile:text-lg'>Websites Projects</h1></Link>
        <Link className='flex gap-2' to={'/websites/projects'}><NavigationIcon icon='link' /><h1 className='text-xl font-bold max-mobile:text-lg'>Websites Projects</h1></Link>
